8e809f81eff4f0eb66651c414e96333c389ba02d
박지용 <qkrwldyd19@gmail.com>
2018-05-19 07:14:57 -0700

    Initial Commit

---
+++ MonsTests/MonsTests.swift
@@ -0,0 +1,36 @@
+//
+//  MonsTests.swift
+//  MonsTests
+//
+//  Created by Jiyong Park on 19/05/2018.
+//  Copyright © 2018 jungwon.sy. All rights reserved.
+//
+
+import XCTest
+@testable import Mons
+
+class MonsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
